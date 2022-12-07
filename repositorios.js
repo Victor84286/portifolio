9224d160415ee04c46ef0cdab3f66e45471f60db
Victor Hugo Rocha Aguiar Pinto <102185271+Victor84286@users.noreply.github.com>
2022-12-07 13:28:55 -0300

    Update repositorios.js

--- repositorios.js
+++ repositorios.js
@@ -4,14 +4,57 @@ repositorios = [
         "nome": "Ibtech",
         "link": "https://ibtech.sendodigital.com",
         "descricao": "Site com informações de alunos de analise e desenvolvimento de sistemas do Ibmec, tecnologias sabidas pelos alunos. Feito em reactJs",
-        "criacao": "14/november/2022",
+        "criacao": "14/novembro/2022",
         "linguagemPrincipal":"JavaScript"
     },{
         "ID": 2,
         "nome": "Aulas de desenvolvimento web",
         "link": "https://github.com/Victor84286/desenvolvimento-web",
         "descricao": "Repositório de todas as aulas de desenvolvimento web da faculdade.",
-        "criacao": "01/august/2022",
+        "criacao": "01/agosto/2022",
         "linguagemPrincipal":"html and css"
+        
+    },{
+        "ID": 3,
+        "nome": "Jogo de pong",
+        "link": "https://github.com/Victor84286/pong",
+        "descricao": "Repositório com a avaliação continuada da matéria de programação estruturada da faculdade. Jogo de pong com limite de 5 pontos corridos ao longo do jogo. Feito em python",
+        "criacao": "09/novembro/2022",
+        "linguagemPrincipal":"python"
+    },{
+        "ID": 4,
+        "nome": "Jogo da memória",
+        "link": "https://github.com/Victor84286/jogo_da_memoria",
+        "descricao": "Repositório com a avaliação final da matéria de programação estruturada da faculdade. Jogo da memória que não aparece mensagem final ao se virar todas as cartas. Feito em python",
+        "criacao": "16/novembro/2022",
+        "linguagemPrincipal":"python"
+    },{
+        "ID": 5,
+        "nome": "Ficha de personagem de dongeons & dragons",
+        "link": "https://github.com/Victor84286/ficha-personagem-d-d",
+        "descricao": "Repositório com a avaliação inicial da matéria de programação estruturada da faculdade. Programa para criação de personagens para D&D em que são lançados 24 dados, sendo 4 para cada atributo, descartando o menor e somando o resto. Em seguida corrige para a raca. No fim pergunta se ficou satisfeito com a ficha e se pretende salvá-la, o arquivo é salvo com o nme do personagem. Feito em python",
+        "criacao": "16/novembro/2022",
+        "linguagemPrincipal":"python"
+    },{
+        "ID": 6,
+        "nome": "Inicio de java",
+        "link": "https://github.com/Victor84286/inicio-Java",
+        "descricao": "Repositório com todos os códigos da aula de programação orientada a objetos. feito em java",
+        "criacao": "15/julho/2022",
+        "linguagemPrincipal":"java"
+    },{
+        "ID": 7,
+        "nome": "Inicio de c",
+        "link": "https://github.com/Victor84286/Inicio-C",
+        "descricao": "Repositório com parte dos códigos da aula de estrutura de dados. feito em c",
+        "criacao": "15/julho/2022",
+        "linguagemPrincipal":"c"
+    },{
+        "ID": 8,
+        "nome": "Estrutura de dados",
+        "link": "https://github.com/Victor84286/Estrutura-de-Dados",
+        "descricao": "Repositório com parte dos códigos da aula de estrutura de dados. feito em c",
+        "criacao": "15/julho/2022",
+        "linguagemPrincipal":"c"
     }
 ]
